80cc403404da77e1d458d7a40410924b3c44c335
Budtawat <69070286@kmitl.ac.th>
2026-07-03 12:24:55 +0700

    Update name.py
    NAme

--- 2981/name.py
+++ 2981/name.py
@@ -1,2 +1,14 @@
-print("Hello Budtawat Sirimajan")
-print("BuSi")
+
+def name(B1,B2):
+    '''function to print names'''
+    print(f"Hello {B1} {B2}")
+B1="Budtawat"
+B2="Sirimajan"
+name(B1, B2)
+
+def nameless(B3):
+    '''function to print names'''
+    print(f"Hello {B3}")
+
+B3="BuSi"
+nameless(B3)
