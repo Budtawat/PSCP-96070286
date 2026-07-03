0a3013f90fbcca5f96c7bb2ba10b21622a4772f5
Budtawat <69070286@kmitl.ac.th>
2026-07-03 12:39:13 +0700

    Update name.py
    nn

--- 2981/name.py
+++ 2981/name.py
@@ -3,10 +3,4 @@ def name(a,b):
     '''function to print names'''
     print(f"Hello {a} {b}")
 name("Budtawat", "Sirimajan")
-
-
-def nameless(c):
-    '''function to print names'''
-    print(f"{c}")
-B3="BuSi"
-nameless(B3)
+name = input("BuSi")
