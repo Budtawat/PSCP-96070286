1d7aff64c12c120d8d309d0c67d9af834f587d4a
Budtawat <69070286@kmitl.ac.th>
2026-07-29 00:47:04 +0700

    Fix variable casing and improve input handling in Elo.py

--- 2997/Elo.py
+++ 2997/Elo.py
@@ -1,3 +1,13 @@
 """Elo"""
-ra = input()
-RB = input()
+RA = int(input())
+RB = int(input())
+player = input()
+
+EA = 1 / (1 + 10 ** ((RB - RA) / 400))
+EB = 1 / (1 + 10 ** ((RA - RB) / 400))
+
+if player == "A":
+    print(f"{EA:.2f}")
+else:
+    print(f"{EB:.2f}")
+    
