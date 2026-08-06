b6ccefa2b2c8ac21ff9dd5c07608d4ec24fbf419
Budtawat <69070286@kmitl.ac.th>
2026-08-06 22:23:36 +0700

    Update Calculator.py

--- 3023/Calculator.py
+++ 3023/Calculator.py
@@ -5,4 +5,7 @@ n = int(input())
 if n == 1:
     print(1)
 else:
-    print(2 * n)
+    digit_count = sum(len(str(i)) for i in range(1, n + 1))
+    
+    total_presses = digit_count + n
+    print(total_presses)
