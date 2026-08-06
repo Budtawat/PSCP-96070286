422d50725c695205e151aafa3fd2d2eea853c593
Budtawat <69070286@kmitl.ac.th>
2026-08-06 22:13:44 +0700

    Update Quandrant.py

--- 3010/Quandrant.py
+++ 3010/Quandrant.py
@@ -2,17 +2,20 @@
 x = int(input())
 y = int(input())
 
-if not x and not y:
-    print("O")
-elif not y:
+if x == 0:
+    if y == 0:
+        print("O")
+    else:
+        print("Y")
+elif y == 0:
     print("X")
-elif not x:
-    print("Y")
-elif x > 0 and y > 0:
-    print("Q1")
-elif x < 0 and y > 0:
-    print("Q2")
-elif x < 0 and y < 0:
-    print("Q3")
-elif x > 0 and y < 0:
-    print("Q4")
+elif x > 0:
+    if y > 0:
+        print("Q1")
+    else:
+        print("Q4")
+else:
+    if y > 0:
+        print("Q2")
+    else:
+        print("Q3")
