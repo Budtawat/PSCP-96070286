ab5003b183031b0eaddcc13e73c1393cb4b03573
Budtawat <69070286@kmitl.ac.th>
2026-08-10 09:27:17 +0700

    a

---
+++ 3101/สถานะน้ำ.py
@@ -0,0 +1,16 @@
+"""สถานะน้ำ"""
+
+temp = int(input())
+unit = input().upper()
+temp_unit = 0
+if unit == "C":
+    temp_unit = temp
+elif unit == "F":
+    temp_unit = (temp - 32) * 5 / 9
+
+if temp_unit <= 0:
+    print("solid")
+elif 0 < temp_unit < 100:
+    print("liquid")
+else:
+    print("gas")
