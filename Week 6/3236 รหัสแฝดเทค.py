b9dc0f92a293b691690d76619ced9268f87478e2
Budtawat <69070286@kmitl.ac.th>
2026-08-14 12:11:40 +0700

    aa

--- Week 6/3236 รหัสแฝดเทค.py
+++ Week 6/3236 รหัสแฝดเทค.py
@@ -1,24 +1,15 @@
 """รหัสแฝดเทค"""
 
-id = int(input())
-std1 = int(input())
-std2 = int(input())
+id1 = int(input())
+std1 = input()
+std2 = input()
 
-list1 = []
-list2 = []
 count = 0
-count2 = 0
-std1 = str(std1)
-std2 = str(std2)
-for i in range(id):
-    list1.append(std1[i])
-    list2.append(std2[i])
-for j in range(id):
-    if int(list1[j]) + int(list2[j]) == 9:
+
+for j in range(id1):
+    if int(std1[j]) + int(std2[j]) != 9:
         count += 1
-    else:
-        count2 += 1
-if count == id:
+if not count:
     print("YES")
 else:
-    print(f"NO {count2}")
+    print(f"NO {count}")
